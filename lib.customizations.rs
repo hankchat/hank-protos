ef92c3f14c9623cc75f7f58fb8ca12389a30e65d
Marc Addeo <marcaddeo@gmail.com>
2024-09-26 10:59:31 -0400

    Update custom ser_de's

--- lib.customizations.rs
+++ lib.customizations.rs
@@ -1,23 +1,38 @@
 // Customizations from hank-protos/lib.Customizations.rs
 pub use hank::*;
 
-// @TODO need to do the deserializer too
 impl serde::ser::Serialize for crate::access_check::AccessCheckChain {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::ser::Serializer,
     {
         use serde::ser::SerializeStruct as _;
-        let mut state = serializer.serialize_struct("AccessCheckChain", 2)?;
+        let mut state = serializer.serialize_struct("AccessCheckChain", 1)?;
         state.serialize_field(
-            "operator",
             crate::access_check::access_check_chain::Operator::try_from(self.operator)
                 .expect("invalid operator")
                 .as_str_name(),
+            &self.checks,
         )?;
-        state.serialize_field("checks", &self.checks)?;
 
         state.end()
+    }
+}
+
+impl<'de> serde::de::Deserialize<'de> for crate::access_check::AccessCheckChain {
+    fn deserialize<D: serde::de::Deserializer<'de>>(d: D) -> Result<Self, D::Error> {
+        let d: std::collections::BTreeMap<
+            String,
+            Vec<crate::access_check::access_check_chain::AccessCheck>,
+        > = std::collections::BTreeMap::deserialize(d)?;
+
+        let (operator, checks) = d.first_key_value().expect("invalid access chain format");
+        Ok(crate::access_check::AccessCheckChain {
+            operator: access_check::access_check_chain::Operator::from_str_name(operator)
+                .expect("invalid access chain operator")
+                .into(),
+            checks: checks.to_vec(),
+        })
     }
 }
 
@@ -39,7 +54,9 @@ mod tests {
                 },
             ],
         };
-        let json = serde_json::to_string_pretty(&chain).unwrap();
-        println!("{}", json);
+        let se_chain = serde_json::to_string_pretty(&chain).unwrap();
+        println!("{}", se_chain);
+        let de_chain: AccessCheckChain = serde_json::from_str(&se_chain).unwrap();
+        dbg!(&de_chain);
     }
 }
